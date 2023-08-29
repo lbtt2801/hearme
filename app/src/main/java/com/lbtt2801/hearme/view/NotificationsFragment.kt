e1e880abb8e4d904685283c63f0b280347ddc50d
Lê Bùi Tấn Trưởng <lebuitantruong@gmail.com>
2023-08-29 13:56:16 +0700

    init hearme app

---
+++ app/src/main/java/com/lbtt2801/hearme/view/NotificationsFragment.kt
@@ -0,0 +1,43 @@
+package com.lbtt2801.hearme.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.lbtt2801.hearme.databinding.FragmentNotificationsBinding
+import com.lbtt2801.hearme.viewmodel.NotificationsViewModel
+
+class NotificationsFragment : Fragment() {
+
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
